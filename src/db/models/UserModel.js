3b1c7f1be4133307e1a70aecc463493d13e48b26
Alisson Ally <alissonbrg32@gmail.com>
2024-10-07 21:49:18 -0300

    Revert "API funcionando, rotas basicas, banco de dados e models construidos"
    This reverts commit 20dd07c274934d933633e95f599e40afff604aa0.

--- src/db/models/UserModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const UserSchema = new mongoose.Schema({
-    username: {required: true, unique: true, type: String},
-    password: {required: true, type: String}
-})
-
-module.exports = mongoose.model('User', UserSchema)
